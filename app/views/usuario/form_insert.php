85ded39da51573d8ba2fe3de86112f01b5665f28
alexamenez08 <alexamenez08@gmail.com>
2025-11-26 18:00:21 -0600

    estancia

--- app/views/usuario/form_insert.php
+++ app/views/usuario/form_insert.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/panel_style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="public/css/crud_style.css?v=<?php echo time(); ?>">
     <title>Registrar Usuario</title>
 </head>
